fd0febec7442efdb9dfc352d33f1f8518a67666e
ysrr27 <ysrr27@hotmail.com>
2017-07-11 14:41:20 -0400

    cambio de version en config.xml para apple

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -59906,7 +59906,7 @@ angular.module('mm.core')
     "app_id" : "com.moodle.moodlemobile",
     "appname": "Moodle Mobile",
     "versioncode" : "2018",
-    "versionname" : "3.3.0",
+    "versionname" : "3.3.1",
     "cache_expiration_time" : 300000,
     "default_lang" : "es",
     "languages": {"ar": "عربي", "bg": "Български", "ca": "Català", "cs": "Čeština", "da": "Dansk", "de": "Deutsch", "el": "Ελληνικά", "en": "English", "es": "Español", "es-mx": "Español - México", "eu": "Euskara", "fa": "فارسی", "fr" : "Français", "he" : "עברית", "hu": "magyar", "it": "Italiano", "ja": "日本語","nl": "Nederlands", "pl": "Polski", "pt-br": "Português - Brasil", "pt": "Português - Portugal", "ro": "Română", "ru": "Русский", "sv": "Svenska", "tr" : "Türkçe", "uk" : "Українська", "zh-cn" : "简体中文", "zh-tw" : "正體中文"},
